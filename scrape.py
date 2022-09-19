eb8f860e038590ba72f2e09cf857d94aa1f5ed0c
TannerTX <tanner_bibb@yahoo.com>
2022-09-19 01:50:38 -0500

    Cleaned up code

--- scrape.py
+++ scrape.py
@@ -14,10 +14,8 @@ def writeTweets(username, keywords, limit):
     tweets = []
 
     if keywords:
-        if "," in keywords:
-            keywords = [ keyword.strip() for keyword in keywords.strip().split(',')]
-
-    keywords = "(" + " OR ".join(keywords) + ")"
+        if "," in keywords: 
+            keywords = "(" + " OR ".join( [keyword.strip() for keyword in keywords.split(',')] ) + ")"
 
     for tweet in sntwitter.TwitterSearchScraper(query + (str(keywords) if keywords else "")).get_items():
         if len(tweets) == limit:
@@ -27,45 +25,37 @@ def writeTweets(username, keywords, limit):
 
     return tweets
 
-
 def create_url():
     user_id = str(input("Enter user ID: "))
     return f"https://api.twitter.com/2/users/{user_id}/following"
 
-
 def get_params():
     return {"user.fields": "created_at"}
-
 
 def bearer_oauth(r):
     r.headers["Authorization"] = f"Bearer {bearer_token}"
     r.headers["User-Agent"] = "v2FollowersLookupPython"
     return r
 
-
-def connect_to_endpoint(url, params):
-    response = requests.request("GET", url, auth=bearer_oauth, params=params)
+def connect_to_endpoint(url):
+    response = requests.request("GET", url, auth=bearer_oauth, params=get_params())
     if response.status_code != 200:
         raise Exception(
-            "Request returned an error: {} {}".format(
-                response.status_code, response.text
-            )
+            f"Request returned an error: {response.status_code} {response.text}"
         )
     return response.json()
-
 
 def main():
 
     url = create_url()
-    params = get_params()
-    json_response = connect_to_endpoint(url, params)
+    json_response = connect_to_endpoint(url)
     tweets = []
 
-    keyword = str(input("Keyword: "))
+    keyword = str(input("Keyword(s) (separate with ,): "))
     limit = int(input("Tweet Limit/Person: "))
 
-    for OBJ in json_response['data']:
-        tweets += writeTweets(OBJ['username'], keyword, limit)
+    for account in json_response['data']:
+        tweets += writeTweets(account['username'], keyword, limit)
 
     df = pd.DataFrame(tweets, columns=['Date', 'Username', 'URL', 'Tweet'])
     df.to_csv("outfile.csv", index=False)
