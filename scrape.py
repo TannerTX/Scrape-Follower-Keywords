aed64c97854fe9f317145d3fce63a7d25f0d40aa
TannerTX <tanner_bibb@yahoo.com>
2022-09-21 14:21:34 -0500

    Cleaned up some code

--- scrape.py
+++ scrape.py
@@ -7,7 +7,8 @@ import re
 
 bearer_token = os.environ.get("BEARER_TOKEN")
 
-def writeTweets(username, keywords, limit):
+
+def writeTweets(username, limit, keywords=""):
 
     query = f"(from:@{username})"
     tweets = []
@@ -16,8 +17,9 @@ def writeTweets(username, keywords, limit):
         limit = 10
 
     if keywords:
-        if "," in keywords: 
-            keywords = "(" + " OR ".join( [keyword.strip() for keyword in keywords.split(',')] ) + ")"
+        if "," in keywords:
+            keywords = "(" + " OR ".join([keyword.strip()
+                                          for keyword in keywords.split(',')]) + ")"
 
     for tweet in sntwitter.TwitterSearchScraper(query + (str(keywords) if keywords else "")).get_items():
         if len(tweets) == limit:
@@ -27,25 +29,35 @@ def writeTweets(username, keywords, limit):
 
     return tweets
 
+
 def create_url():
-    user_id = str(input("Enter user ID: "))
+    user_id = str(input("*REQUIRED* Enter user ID: "))
+
+    if not user_id:
+        return None
+
     return f"https://api.twitter.com/2/users/{user_id}/following"
+
 
 def get_params():
     return {"user.fields": "created_at"}
+
 
 def bearer_oauth(r):
     r.headers["Authorization"] = f"Bearer {bearer_token}"
     r.headers["User-Agent"] = "v2FollowersLookupPython"
     return r
 
+
 def connect_to_endpoint(url):
-    response = requests.request("GET", url, auth=bearer_oauth, params=get_params())
+    response = requests.request(
+        "GET", url, auth=bearer_oauth, params=get_params())
     if response.status_code != 200:
         raise Exception(
             f"Request returned an error: {response.status_code} {response.text}"
         )
     return response.json()
+
 
 def main():
 
@@ -63,17 +75,24 @@ def main():
         )
 
     else:
-        json_response = connect_to_endpoint(create_url())
+
+        url = create_url()
+
+        if not url:
+            print("| ID is required! |")
+            quit()
+
+        json_response = connect_to_endpoint(url)
         tweets = []
 
-        keyword = str(input("Keyword(s) (separate with ,): "))
-        limit = int(input("Tweet Limit/Person: "))
+        keywords = str(input("*OPTIONAL* Keyword(s) (separate with ,): "))
+        limit = int(input("*OPTIONAL* Tweet Limit/Person: "))
 
         for account in json_response['data']:
-            tweets += writeTweets(account['username'], keyword, limit)
+            tweets += writeTweets(account['username'], limit, keywords)
 
         df = pd.DataFrame(tweets, columns=['Date', 'Username', 'URL', 'Tweet'])
-        df.to_csv("outfile.csv", index=False)
+        df.to_csv("results.csv", index=False)
 
 
 if __name__ == "__main__":
